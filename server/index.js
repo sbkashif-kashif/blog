923c04d2e4b1cf663abca8a709e8190020dc946c
sbkashif-kashif <kashif@sbkashif.com>
2024-07-08 23:07:17 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import userRoute from '../routes/user.js'
+
+// Get the directory name of the current module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Specify the absolute path to the .env file
+dotenv.config({ path: path.resolve(__dirname, '../config/.env') });
+
+const username = process.env.DBUSER;
+const password = process.env.DBPASS;
+
+// Check if DB, username, and password are loaded correctly
+if (!process.env.DB || !username || !password) {
+    console.error("Environment variables are not loaded correctly. Please check your .env file.");
+    process.exit(1); // Exit the application if environment variables are not loaded
+}
+
+const database = process.env.DB.replace('<username>', username).replace('<password>', encodeURIComponent(password));
+
+mongoose.connect(database).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((err) => {
+    console.error("Not Connected to MongoDB", err);
+});
+
+const app = express();
+
+const port = process.env.PORT || 3030;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+app.use('/api/v1/', userRoute);
